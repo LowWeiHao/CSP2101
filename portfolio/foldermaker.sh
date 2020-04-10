174844acaf88a48a9bc2d648349e1902ee8e4ce8
LowWeiHao <weitei2000@gmail.com>
2020-04-10 20:24:32 +0800

    finished

--- portfolio/foldermaker.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-read -p "type the name of the folder you would like to create:" folderName
-#ask user the name of folder that user want to make.
-mkdir "$folderName"
-#created folder with the name user type in.
